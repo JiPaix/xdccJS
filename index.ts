387ada56efef842b834521ec03dcc75cc79600c8
JiPaix <jipai13@gmail.com>
2020-04-01 01:25:06 +0200

    upgrade and linted

--- index.ts
+++ index.ts
@@ -1,17 +1,20 @@
 import * as IRC from 'irc-framework';
-import customEvents from 'eventemitter3';
 import fs from 'fs'
 import path from 'path'
 import net from 'net'
 
 type packageNumber = string | number;
 
-export = class XDCC extends IRC.Client {
+class XDCC extends IRC.Client {
     nick: string
     chan: string
     path: string
-    constructor(parameters: { host: string, port: number, nick: string, chan: string, path: string }) {
+    verbose: boolean
+    disconnect: boolean
+    constructor(parameters: { host: string; port: number; nick: string; chan: string; path: string; disconnect?: boolean; verbose?: boolean }) {
         super()
+        this.verbose = parameters.verbose || false
+        this.disconnect = parameters.disconnect || false
         this.nick = this.nickRandomizer(parameters.nick)
         this.chan = this.HashTag(parameters.chan)
         this.connect({
@@ -19,82 +22,127 @@ export = class XDCC extends IRC.Client {
             port: parameters.port,
             nick: this.nick,
             encoding: 'utf8',
+            // eslint-disable-next-line @typescript-eslint/camelcase
             auto_reconnect: false,
         })
-        this.customEvents = new customEvents()
-        this.path = path.join(path.resolve(__dirname, '../'), parameters.path)
+        this.path = path.join(path.resolve('./'), parameters.path)
         fs.mkdirSync(this.path, { recursive: true })
         this.live()
     }
-    private live() {
+    private live(): void {
+        // eslint-disable-next-line @typescript-eslint/no-this-alias
+        const self = this
         this.on('connected', () => {
-            let channel = this.channel(this.chan);
+            const channel = this.channel(this.chan);
             channel.join();
+
+            self.emit('xdcc-ready')
+            if (this.verbose) { console.log(`connected and joined ${this.chan}`) }
         });
-        this.customEvents.on('request', (args: { target: string, packet: packageNumber }) => {
-            let channel = this.channel(this.chan);
+        this.on('request', (args: { target: string; packet: packageNumber }) => {
             this.say(args.target, 'xdcc send ' + args.packet)
+            if (this.verbose) { console.log(`/MSG ${args.target} xdcc send ${args.packet} `) }
         })
-        this.on('ctcp request', (resp: any) => {
-            let self = this
-            let fileInfo = this.parseCtcp(resp.message)
-            let file = fs.createWriteStream(fileInfo.filePath)
+        this.on('ctcp request', (resp: { [prop: string]: string }): void => {
+            if (resp.message === null || typeof resp.message !== 'string') {
+                throw new TypeError('CTCP : unexpected response.')
+            }
+            const fileInfo = this.parseCtcp(resp.message)
+            const file = fs.createWriteStream(fileInfo.filePath)
             file.on('open', () => {
                 let received = 0
-                let sendBuffer = Buffer.alloc(4)
-                let client = net.connect(fileInfo.port, fileInfo.ip, () => {
-                    self.customEvents.emit('download start')
+                const sendBuffer = Buffer.alloc(4)
+                let slowdown: NodeJS.Timeout = setInterval(() => { throw new Error(`not receiving any data`) }, 10000)
+                const client = net.connect(fileInfo.port, fileInfo.ip, () => {
+                    self.emit('download start')
+                    if (this.verbose) { console.log(`download starting: ${fileInfo.file} `) }
                 })
                 client.on('data', (data) => {
                     file.write(data)
                     received += data.length
                     sendBuffer.writeUInt32BE(received, 0)
                     client.write(sendBuffer)
-                    self.customEvents.emit('downloading', received, fileInfo)
+                    if (received === data.length) {
+                        clearInterval(slowdown)
+                        slowdown = setInterval(() => {
+                            self.emit('downloading', received, fileInfo)
+                            if (this.verbose) { process.stdout.write(`downloading : ${this.formatBytes(received)} / ${this.formatBytes(fileInfo.length)}\r`) }
+                        }, 1000)
+                    } else if (received === fileInfo.length) {
+                        clearInterval(slowdown)
+                    }
                 })
                 client.on('end', () => {
                     file.end()
-                    self.customEvents.emit('downloaded', fileInfo)
+                    self.emit('downloaded', fileInfo)
+                    if (this.verbose) { console.log(`downloading done: ${file.path}`) }
+                    if (this.disconnect) { self.quit('muybueno') }
                 })
                 client.on('error', (err) => {
-                    self.customEvents.emit('download error', err, fileInfo)
+                    self.emit('download error', err, fileInfo)
                     file.end()
+                    if (this.verbose) { console.log('download error', err) }
                 })
             })
         })
     }
-    public send(target: string, packet: packageNumber) {
+    public send(target: string, packet: packageNumber): void {
         packet = this.HashTag(packet)
-        this.customEvents.emit('request', { target, packet })
+        this.emit('request', { target, packet })
     }
-    private nickRandomizer(nick: string) {
-        return nick + Math.floor(Math.random() * 9999) + 1
+    private nickRandomizer(nick: string): string {
+        if (nick.length > 6) {
+            nick = nick.substr(0, 6)
+        }
+        return nick + Math.floor(Math.random() * 999) + 1
+    }
+    private formatBytes(bytes: number, decimals = 2): string {
+        if (bytes === 0) return '0 Bytes'
+        const k = 1024
+        const dm = decimals < 0 ? 0 : decimals
+        const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+        const i = Math.floor(Math.log(bytes) / Math.log(k))
+        return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i]
     }
     private HashTag(value: packageNumber): string {
         if (typeof value === 'string') {
-            let chan: string = value
-            if (chan.charAt(0) === '#') {
-                return chan
-
+            if (RegExp(/(^[0-9]+$|^#[0-9]+$)/g).test(value) || RegExp(/(^[A-z]+$|^#[A-z]+$)/g).test(value)) {
+                if (value.charAt(0) === '#') {
+                    return value
+                } else {
+                    return `#${value}`
+                }
             } else {
-                return '#' + chan
+                throw new TypeError(`Package: string must match this pattern: /(^[0-9]+$|^#[0-9]+$)/g`)
             }
         } else if (typeof value === 'number') {
-            let pack: string = '#' + value
-            return pack
+            if (value % 1 === 0) {
+                return `#${value.toString()}`
+            } else {
+                throw new TypeError(`Package: number must be an integer.`)
+            }
+
+        } else {
+            throw new TypeError(`Package: expect package to be a number or a string matching this pattern: /(^[0-9]+$|^#[0-9]+$)/g`)
         }
-        throw new Error('unreachable')
     }
-    private uint32ToIP(n: number) {
-        var byte1 = n & 255
+    private uint32ToIP(n: number): string {
+        const byte1 = n & 255
             , byte2 = ((n >> 8) & 255)
             , byte3 = ((n >> 16) & 255)
             , byte4 = ((n >> 24) & 255);
-
         return byte4 + "." + byte3 + "." + byte2 + "." + byte1;
     }
-    private parseCtcp(text: any) {
-        let parts = text.match(/(?:[^\s"]+|"[^"]*")+/g);
+    private parseCtcp(text: string): { file: string; filePath: string; ip: string; port: number; length: number } {
+        const parts = text.match(/(?:[^\s"]+|"[^"]*")+/g);
+        if (!parts) { throw new TypeError(`CTCP : received unexpected msg : ${text}`) }
+        if (
+            parts.some((index) => {
+                index === null
+            })
+        ) {
+            throw new TypeError(`CTCP : received unexpected msg : ${text}`)
+        }
         return {
             file: parts[2].replace(/"/g, ''),
             filePath: path.normalize(this.path + '/' + parts[2].replace(/"/g, '')),
@@ -104,3 +152,5 @@ export = class XDCC extends IRC.Client {
         };
     }
 }
+
+export = XDCC
