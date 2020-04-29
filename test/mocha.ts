bc780b03fbc8b7b0aaafba0a00b20e72c418eef0
JiPaix <jipai13@gmail.com>
2020-04-29 19:21:02 +0200

    modified event with new event names

--- test/mocha.ts
+++ test/mocha.ts
@@ -1,12 +1,13 @@
 import 'mocha'
-import {
+import
+{
     expect
 } from 'chai'
 import XDCC from '../'
-
+const XDCC2 = require( '../' ).default
 let start: XDCC
 
-const XDCC2 = require('../').default
+
 
 const args = {
     host: 'irc.rizon.net',
@@ -19,41 +20,58 @@ const args = {
 }
 
 
-describe('import and require', () => {
-    it('import', () => {
-        expect(XDCC).to.be.a('function')
-    })
-    it('require', () => {
-        expect(XDCC2).to.be.a('function')
-    })
-})
+describe( 'import and require', () =>
+{
+    it( 'import', () =>
+    {
+        expect( XDCC ).to.be.a( 'function' )
+    } )
+    it( 'require', () =>
+    {
+        expect( XDCC2 ).to.be.a( 'function' )
+    } )
+} )
 
-describe('initialize', () => {
-    before(function (done) {
-        start = new XDCC(args)
-        done()
-    })
-    it('connect', function (done) {
-        this.timeout(10000)
-        start.on('xdcc-ready', () => {
+describe( 'initialize', () =>
+{
+    before( function ( done )
+    {
+        start = new XDCC( args )
+        if ( start instanceof XDCC )
+        {
             done()
-        })
-    })
-    it('download()', function (done) {
-        start.on('request', (res) => {
-            if (res.target === 'JiPaix' && res.packet === '#1') {
+        }
+
+    } )
+    it( 'connect', function ( done )
+    {
+        this.timeout( 10000 )
+        start.on( 'xdcc-ready', () =>
+        {
+            done()
+        } )
+    } )
+    it( 'download()', function ( done )
+    {
+        start.on( 'request', ( res: { target: string; packet: string } ) =>
+        {
+            if ( res.target === 'JiPaix' && res.packet === '#1' )
+            {
                 done()
             }
-        })
-        start.download('JiPaix', 1)
-    })
-    it('downloadBatch()', function (done) {
-        start.on('request batch', (res) => {
-            if (res.target === 'JiPaix' && [2, 3, 4, 5, 9].every((val, i, arr) => val === res.packet[i])) {
+        } )
+        start.download( 'JiPaix', 1 )
+    } )
+    it( 'downloadBatch()', function ( done )
+    {
+        start.on( 'request-batch', ( res: { target: string; packet: number[] } ) =>
+        {
+            if ( res.target === 'JiPaix' && [ 2, 3, 4, 5, 9 ].every( ( val, i, arr ) => val === res.packet[ i ] ) )
+            {
                 start.quit()
                 done()
             }
-        })
-        start.downloadBatch('JiPaix', '2-5 , 9')
-    })
-})
+        } )
+        start.downloadBatch( 'JiPaix', '2-5 , 9' )
+    } )
+} )
