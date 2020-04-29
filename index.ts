e749864329d36b8197292c8ebe39f5ebefd81638
JiPaix <jipai13@gmail.com>
2020-04-29 21:15:11 +0200

    removed always false checking

--- index.ts
+++ index.ts
@@ -440,7 +440,7 @@ export default class XDCC extends Client
     private parseCtcp ( text: string ): FileInfo
     {
         const parts = text.match( /(?:[^\s"]+|"[^"]*")+/g )
-        if ( parts === null || typeof parts === 'undefined' ) { throw new TypeError( `CTCP : received unexpected msg : ${ text }` ) }
+        if ( parts === null ) { throw new TypeError( `CTCP : received unexpected msg : ${ text }` ) }
         if (
             parts.some( ( val ) =>
             {
