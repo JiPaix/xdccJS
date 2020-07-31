2989e5cb0ab7b1e8839737f20bdbad11ab915d77
JiPaix <jipai13@gmail.com>
2020-07-31 22:58:46 +0200

    added missing dep

--- examples/vlc.js
+++ examples/vlc.js
@@ -1,4 +1,6 @@
 // This example will start vlc.exe then play the video while it's downloading. (videos aren't skippable)
+const XDCC = require('xdccjs').default
+
 const opts = {
   host: 'irc.server.net',
   path: false,
@@ -8,14 +10,15 @@ const xdccJS = new XDCC(opts)
 
 // Start VLC
 const { spawn } = require('child_process')
+const path = require('path')
+
 const vlcPath = path.normalize('C:\\Program Files\\VideoLAN\\VLC\\vlc.exe')
 const vlc = spawn(vlcPath, ['-'])
 
 xdccJS.on('ready', () => {
   const Job = xdccJS.download('bot', 155)
-})
-
-// send data to VLC that plays the file
-Job.on('pipe', stream => {
-  stream.pipe(vlc.stdin)
+  // send data to VLC that plays the file
+  Job.on('pipe', stream => {
+    stream.pipe(vlc.stdin)
+  })
 })
