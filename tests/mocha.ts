3e34f7c82dc2433dacb2bbd74e134af06258a479
JiPaix <jipai13@gmail.com>
2021-07-24 11:12:03 +0200

    ci: :bug: fix download errors resulting in test hang

--- tests/mocha.ts
+++ tests/mocha.ts
@@ -60,11 +60,6 @@ const args2: Params = {
   passivePort: [5001]
 }
 
-
-
-// let A: Job
-// let B: Job
-
 let xdccJS1: XDCC
 let xdccJS2: XDCC
 
@@ -147,7 +142,9 @@ let size:number
 describe('RealLife', () => {
   it('download', function (done) {
     this.timeout(10000)
-    xdccJS1.download(process.env.BOT1, 1).once('downloaded', ()=>done())
+    const job = xdccJS1.download(process.env.BOT1, 1)
+    job.once('downloaded', ()=>done())
+    job.on('error', (err) => done(err))
   })
 
   it('quit', function (done) {
@@ -158,7 +155,6 @@ describe('RealLife', () => {
 
 
 let downloadInfo: { bot: string; pack: string }
-const ignore = [process.env.IGNORE1, process.env.IGNORE2, process.env.IGNORE3]
 
 describe('RealLife#2', () => {
   it('Reading messages', function (done) {
@@ -171,7 +167,6 @@ describe('RealLife#2', () => {
             if(!downloadInfo) {
               downloadInfo = { bot: ev.nick, pack: pack[0].replace('#', '') }
             }
-            
           }
         }
       }
@@ -187,7 +182,7 @@ describe('RealLife#2', () => {
     const job = xdccJS2.download(downloadInfo.bot, downloadInfo.pack)
 
     xdccJS2.on('error', e => {
-      console.error(e)
+      done(e)
     })
 
     job.on('pipe', stream => {
@@ -225,179 +220,3 @@ describe('RealLife#2', () => {
     }
   })
 })
-
-// describe('initialize', () => {
-//   before(function (done) {
-//     start = new XDCC(args)
-//     done()
-//   })
-//   it('connect', function (done) {
-//     this.timeout(10000)
-//     start.once('ready', () => {
-//       done()
-//     })
-//   })
-// })
-// let job: void | Job | Job[]
-
-// describe('Jobify', () => {
-//   it('download()', function (done) {
-//     A = start.download('JiPaix', '1-2,5')
-//     start.download('JiPaix', [25])
-//     start.download('JiPaix', ['26'])
-//     start.download('JiPaixx', 2)
-//     if (A.show().now === 1 && A.show().queue[0] === 2) {
-//       done()
-//     }
-//   })
-//   it('register job', function (done) {
-//     if (A.queue.length === 4) {
-//       const jobs = start.jobs()
-//       if (isArray(jobs)) {
-//         if (jobs.length == 2) {
-//           done()
-//         }
-//       }
-//     }
-//   })
-//   it('display job status', function (done) {
-//     job = start.jobs()
-//     if (job) {
-//       if (isArray(job)) {
-//         if (!job[0].isDone()) {
-//           done()
-//         }
-//       }
-//     }
-//   })
-//   it('cancel', function (done) {
-//     if (job) {
-//       if (isArray(job)) {
-//         if (!job[0].cancel) {
-//           done()
-//         }
-//       }
-//     }
-//   })
-// })
-
-// describe('Connexion', () => {
-//   it('can-quit', function (done) {
-//     this.timeout(0)
-//     start.once('can-quit', () => {
-//       done()
-//     })
-//   })
-// })
-// let size: number
-// describe('RealLife', () => {
-//   it('really download', function (done) {
-//     this.timeout(0)
-//     B = start.download(process.env.GIN, 1)
-//     B.on('downloaded', f => {
-//       if (fs.lstatSync(f.filePath).size === f.length) {
-//         size = f.length
-//         done()
-//       }
-//     })
-//   })
-//   it('resume', function (done) {
-//     this.timeout(0)
-//     const C = start.download(process.env.GIN, 1)
-//     C.on('downloaded', f => {
-//       if (fs.lstatSync(f.filePath).size === size) {
-//         done()
-//       }
-//     })
-//   })
-//   it('quit', function (done) {
-//     start.quit()
-//     done()
-//   })
-// })
-
-// let start2: XDCC
-// const args2: Params = {
-//   host: process.env.SERVER2,
-//   chan: [process.env.CHAN1, process.env.CHAN2],
-//   retry: 0,
-//   verbose: true,
-//   randomizeNick: true,
-//   path: false,
-//   passivePort: Array.from(Array(5050 + 1).keys()).slice(5010),
-// }
-
-// let downloadInfo: { bot: string; pack: string }
-// describe('RealLife#2', () => {
-//   it('connect', function (done) {
-//     this.timeout(0)
-//     start2 = new XDCC2(args2)
-//     start2.on('ready', () => {
-//       start2.irc.on('message', ev => {
-//         if (downloadInfo) {
-//           const ignore = ['NickServ', 'TheSource', 'TS-Search']
-//           if (!ignore.includes(ev.nick)) {
-//             if (ev.target === start2.irc.user.nick) {
-//               console.error(ev.message)
-//             }
-//           }
-//         }
-//         if (ev.nick.includes('|P|')) {
-//           if (ev.message.includes('#')) {
-//             const pack = ev.message.match(/#\w+/g)
-//             if (pack) {
-//               if (!downloadInfo) {
-//                 downloadInfo = { bot: ev.nick, pack: pack[0].replace('#', '') }
-//               }
-//             }
-//           }
-//         }
-//       })
-//       setTimeout(() => {
-//         done()
-//       }, 65 * 1000)
-//     })
-//   })
-//   it('download passive pipe', function (done) {
-//     this.timeout(0)
-//     const job = start2.download(downloadInfo.bot, downloadInfo.pack)
-
-//     start2.on('error', e => {
-//       console.error(e)
-//     })
-
-//     job.on('pipe', stream => {
-//       if (stream instanceof PassThrough) {
-//         done()
-//       } else {
-//         console.error('not instance of PassThrough')
-//         process.exit(1)
-//       }
-//     })
-//   })
-
-//   it('cancel()', function (done) {
-//     this.timeout(0)
-//     const JobB = start2.jobs(downloadInfo.bot)
-//     if (JobB instanceof Job) {
-//       if (JobB.cancel) {
-//         JobB.cancel()
-//         JobB.on('cancel', () => {
-//           done()
-//         })
-//       } else {
-//         console.error('not cancellable')
-//         process.exit(1)
-//       }
-//     } else {
-//       console.error('not instance of Job')
-//       process.exit(1)
-//     }
-//   })
-//   it('has no job', function (done) {
-//     start2.quit()
-//     if (!start2.jobs()) {
-//       done()
-//     }
-//   })
-// })
