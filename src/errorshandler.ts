0f2e50bba058c2a766c8f56640b6a43a81c22cf1
JiPaix <jipai13@gmail.com>
2021-08-16 21:40:38 +0200

    style: send unmodified string with JobError  [skip ci]

--- src/errorshandler.ts
+++ src/errorshandler.ts
@@ -1,8 +1,6 @@
-import Connect from './connect';
-
 export default class JobError extends Error {
   constructor(message: string) {
-    const newMessage = Connect.replace(message);
+    const newMessage = message;
     super(newMessage);
   }
 }
